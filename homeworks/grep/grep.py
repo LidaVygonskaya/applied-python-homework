64dec27276d11e3069f596977baa643687464d3f
Lida <Vygonskaya.lyu@phystech.edu>
2019-01-04 19:36:12 +0700

    first commit

--- homeworks/grep/grep.py
+++ homeworks/grep/grep.py
@@ -1,6 +1,7 @@
 
 import argparse
 import sys
+import re
 
 
 def output(line):
@@ -8,10 +9,69 @@ def output(line):
 
 
 def grep(lines, params):
+    def add_context_lines(line_number, before=True):
+        if before:
+            for i in range(1, line_number + 1):
+                if line_index - i >= 0 and line_index - i + 1 not in result_output:
+                    result_output.update({line_index - i + 1: {'line': lines[line_index - i], 'isFounded': False}})
+        else:
+            for i in range(1, line_number + 1):
+                if line_index + i < len(lines) and line_index + i + 1 not in result_output:
+                    result_output.update({line_index + i + 1: {'line': lines[line_index + i], 'isFounded': False}})
+
+    result_output = {}
+    regexp = create_regexp(params)
     for line in lines:
         line = line.rstrip()
-        if params.pattern in line:
-            output(line)
+
+        if (re.search(regexp, line) and not params.invert) or (not re.search(regexp, line) and params.invert):
+            line_index = lines.index(line)
+            result_output.update({line_index + 1: {'line': line, 'isFounded': True}})
+
+            if params.context:
+                add_context_lines(params.context)
+                add_context_lines(params.context, False)
+
+            if params.before_context:
+                add_context_lines(params.before_context)
+
+            if params.after_context:
+                add_context_lines(params.after_context, False)
+
+    for line in format_output(params, result_output):
+        output(line)
+
+
+def format_output(params, lines):
+    def sort_dict(lines):
+        sorted_dict = {}
+        for key in sorted(lines.keys()):
+            sorted_dict.update({key: lines[key]})
+        return sorted_dict
+
+    lines = sort_dict(lines)
+    if params.count:
+        if lines:
+            return [str(len(lines))]
+        else:
+            return [str(0)]
+
+    if params.line_number:
+        return [f"{key}:{value['line']}" if value['isFounded'] else f"{key}-{value['line']}" for key, value in lines.items()]
+
+    else:
+        return [value['line'] for value in lines.values()]
+
+
+def create_regexp(params):
+    regexp = re.escape(params.pattern)
+    regexp = re.sub(r'\\\?', '.', regexp)
+    regexp = re.sub(r'\\\*', '.*', regexp)
+
+    if params.ignore_case:
+        return re.compile(regexp, re.IGNORECASE)
+    else:
+        return re.compile(regexp)
 
 
 def parse_args(args):
